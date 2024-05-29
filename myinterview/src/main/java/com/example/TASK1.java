324dc896c2928c8b3c12839a8a8c49c49733b66a
lucassilvap <lucassilvap148@gmail.com>
2024-05-29 16:22:34 -0300

    TASK 1 FINALIZADO

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -1,4 +1,4 @@
-package com.example;
+package main.java.com.example;
 import java.util.Scanner;
 /**
  * 
@@ -13,6 +13,20 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
-     
- 
+
+    public static boolean isPalindrome(String phrase){
+        // remove todos os espaços da frase e coloca tudo em letra maiscula
+        phrase = phrase.replaceAll("\\s+", "").toUpperCase();
+
+        // pega o tamanho da frase
+        int lengthPhrase = phrase.length();
+
+        // verifica se a frase é um palindrome
+        for (int i = 0; i < lengthPhrase / 2; i++) {
+            if (phrase.charAt(i) != phrase.charAt(lengthPhrase - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
