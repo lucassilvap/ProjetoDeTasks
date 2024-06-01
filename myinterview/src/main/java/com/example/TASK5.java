49b56b5aa4f21691e2b88b43e6b2de49facffc3a
lucassilvap <lucassilvap148@gmail.com>
2024-06-01 12:14:35 -0300

    five tasks finished

--- myinterview/src/main/java/com/example/TASK5.java
+++ myinterview/src/main/java/com/example/TASK5.java
@@ -218,7 +218,7 @@ abstract class PersonSpecification{
 
     /*Faz a consulta por profissao */
     private static Specification<Person> Byprofession(String profession){
-        return (root, cq, cb) -> cb.like(cb.upper(root.get("profession")),  profession.toUpperCase());
+        return (root, cq, cb) -> cb.like(cb.upper(root.get("profession")), "%" + profession.toUpperCase()  + "%");
     }
 
     /*Verifica quais parametros estão presentes e faz a contetanação das consultas */
